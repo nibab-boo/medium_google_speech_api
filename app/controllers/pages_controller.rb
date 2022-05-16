545a4fd15f95373fa6b84af210b0d892c9a73408
Babin Bohora <bohorababin1@gmail.com>
2022-05-16 14:41:21 +0900

    readme updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     uploaded_file = params["files"]
     audio_file = uploaded_file.read
 
-    # Call private method speech_to_text responsible for transcription
+    # Call private method speech_to_text doing the transcription
     google_text_obj = speech_to_text(audio_file)
 
     # Retrieve the text from the response.
@@ -22,16 +22,15 @@ class PagesController < ApplicationController
 
   # You can put this in separate file as a part of module.
   def speech_to_text(audio_file)
-    # reading the credentials from credentials.json
-    # For the project you want to production, you might want to put it inside .env 
+    # read the credentials from credentials.json
+    # For the project you want deploy to production, you might need put it inside .env 
     credentials = JSON.parse(File.read("app/controllers/concerns/credentials.json"))
     
-    # Creating a new client with credentials.
+    # Create a new client with credentials.
     client = ::Google::Cloud::Speech::V1::Speech::Client.new do |config|
       config.credentials = credentials
     end
 
-    # You shouldn't need to manual setup everything since we don't know exact hertz and everything.
     # For more config options and best practices, check https://cloud.google.com/speech-to-text/docs/best-practices
     config = {
       language_code: 'ja-JP',
